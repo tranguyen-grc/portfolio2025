4725ba98242c10ef2543c3aaa77eb41125e6d791
tranguyen-grc <chiptra292@gmail.com>
2025-08-15 14:40:23 -0700

    added vercel.json

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   // switch to "/" for dev
-  base: process.env.VITE_BASE_PATH || "/portfolio2025",
+  base: process.env.VITE_BASE_PATH || "/",
   plugins: [react(), tailwindcss()],
 });
